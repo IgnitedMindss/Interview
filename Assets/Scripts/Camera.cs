30ae9a1369c5f35381f5ec75f07ea564522b2e37
Shubhro <shubhrojyotikabiraj@gmail.com>
2022-08-12 15:01:54 +0530

    daveai assignment

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    GameObject lookAtPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lookAtPoint = GameObject.Find("/Player/LookAtPoint");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(lookAtPoint.transform);
+    }
+}
